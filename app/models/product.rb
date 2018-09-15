3572937993b5fd0bbf474796071a18aab9212d95
charles-henri poniard <c.poniard@gmail.com>
2018-09-15 14:13:30 +0200

    fix pbl model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,7 @@
 class Product < ApplicationRecord
-<<<<<<< HEAD
+
   belongs_to :productor
-=======
   belongs_to :user
   has_many :order_products
->>>>>>> 54b5224a31439a3a61ea636e6b2ba6dbe3ecec36
+
 end
