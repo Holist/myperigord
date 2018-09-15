7c406c232df81f21cb7bf699b8762450f2a7d8f4
Claudine Pradal <claudinecartailler@gmail.com>
2018-09-15 16:45:40 +0200

    install gem monetize

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :productor
   has_many :order_products, dependent: :destroy
-
+  monetize :price_cents
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_130629) do
+ActiveRecord::Schema.define(version: 2018_09_15_144329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,10 +54,10 @@ ActiveRecord::Schema.define(version: 2018_09_15_130629) do
     t.string "name"
     t.text "description"
     t.string "photo"
-    t.integer "price"
     t.bigint "productor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price_cents", default: 0, null: false
     t.index ["productor_id"], name: "index_products_on_productor_id"
   end
 
