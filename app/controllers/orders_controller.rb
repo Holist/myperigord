91cc9b8c76ebdb3ddf73e4d474693e095870ac9e
Claudine Pradal <claudinecartailler@gmail.com>
2018-09-15 19:42:58 +0200

    debug order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,10 @@
-class OrderController < ApplicationController
+class OrdersController < ApplicationController
   def index
     @orders = Order.all
   end
 
   def show
+    @order = current_order
     @order_products = current_order.order_products
   end
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -2,7 +2,6 @@ class OrderProduct < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
-  validates :quantity, presence: true, { numericality :true, greater_than: 0 }
   validates :product, presence: true
   validates :order, presence: true
 
