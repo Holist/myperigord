44d8d57d0b94619b99b9c27fc0d8649cfb86ba67
ClaudineP <34744530+ClaudineP435433@users.noreply.github.com>
2018-09-15 18:13:12 +0200

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    @order_item = current_order.order_items.new
+    @order_product = current_order.order_products.new
   end
 
   def show
