d471cd60e8c7763c6e689e5ef972776fc98d477e
Claudine Pradal <claudinecartailler@gmail.com>
2018-09-16 12:07:07 +0200

    update visual for devise sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_order
 
   def current_order
@@ -8,6 +9,24 @@ class ApplicationController < ActionController::Base
     else
       Order.new(customer: current_user.specific)
     end
+  end
+
+  def configure_permitted_parameters
+    user_data = [
+      :first_name,
+      :last_name,
+      :address,
+      :phone,
+      :siret,
+      :compagny,
+      :email,
+      :password,
+      :actable_id,
+      :actable_type
+    ]
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: user_data)
+    devise_parameter_sanitizer.permit(:account_update, keys: user_data)
   end
 
 end
